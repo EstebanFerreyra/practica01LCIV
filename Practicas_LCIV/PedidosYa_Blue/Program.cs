ce65fc7083a523d5b43aa73c744f7734cf698a36
EstebanFerreyra <eferreyra098@gmail.com>
2023-08-23 15:15:05 -0300

    Comentarios en Program aclarando porque aplicamos los patrones de dieño usados

--- Practicas_LCIV/PedidosYa_Blue/Program.cs
+++ Practicas_LCIV/PedidosYa_Blue/Program.cs
@@ -7,3 +7,10 @@ dronManager.AgregarDron();
 dronManager.AgregarDron();
 
 dronManager.MostrarDrones();
+
+// En este pequeño programa intentamos aplicar patrones de diseño como factory method y inyeccion de dependecias.
+
+// Factory method lo aplicamos para la creacion de repartidores (en este caso tipo Drone), lo que en un futuro
+// nos facilitaria la implementacion de otro tipo de repartidor, haciendo el codigo mas escalable y mantenible.
+
+// Inyeccion de dependencias lo utilizamos para inyectar una RepartidorFactory en la clase DroneManager.
